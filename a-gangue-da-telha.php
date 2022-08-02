18ba94259b789c23e4ccb9fa24c07d44f71afdbf
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-08-02 12:37:29 -0300

    a-gangue-da-telha.php

---
+++ a-gangue-da-telha.php
@@ -0,0 +1,127 @@
+<head>
+    <link rel="shortcut icon" href="imagens/log.png" type="image x-icon">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"content="">
+        <meta property="og:site_name" content="Vacilou, virou Crônica! | O que vacila, vira Crônica!" />
+    <meta property="og:url" content="https://vacilouviroucronica.com/a-gangue-da-telha.php" />
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://vacilouviroucronica.com/a-gangue-da-telha.php" />
+    <meta name="author" content="Anderson Lietz">
+    <meta property="og:type" content="article" />
+<title>A Gangue da Telha | Vacilou, virou Crônica!</title>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+</head>
+<body>
+  <header>
+    <?php include "header.php";
+    ?>
+</header>
+              <main>
+                  
+              <article>
+                      <h1>A Gangue da Telha</h1>
+                      <img src="imagens/a-gangue-da-telha.jpg" class="imagem">
+                      <a href="https://www.pexels.com/pt-br/foto/taipei-arquitetura-chinesa-vermelho-cobertura-5401232/" >Foto de Henry & Co.</a></img>
+   <section class="aumentarediminuirfonte">
+  <!----><!-- DIMINUIR FONTE -->
+  <span id="diminuir-fonte">
+<a title="Diminuir Fonte" alt="Diminuir">A-</a>
+</span>
+
+<!-- AUMENTAR FONTE -->
+<span id="aumentar-fonte">
+<a title="Aumentar Fonte" alt="Aumentar">A+</a>
+</span>
+</section>
+    <div class="espaco"></div>
+<section id="content">
+<p>No Uber o homem pensava em como iria aproveitar sua folga. Planejava chegar em casa, abrir uma garrafa de vinho ou uma latinha de cerveja, talvez coar café. Estava farto de viajar, e o lugar em que mais desejava passar alguns dias era em casa. Não havia praia no mundo que ele deseja-se agora mais do que passar uma tarde na poltrona da sala, lendo alguns livros que havia comprado durante suas viagens e reservou para apreciá-los em casa, sem pressa e sozinho.</p>
+
+<p>Ao chegar no destino o carro parou, o motorista tirou as malas com agilidade e colocou na calçada, enquanto o homem se espreguiçava. O Uber se despediu e foi embora, ele pegou duas malas e atravessou a rua. Ao abrir o portão e buscar as outras malas, parou um momento e ficou observando a cunheira da casa.</p>
+
+<p>— Cadê o meu telhado? — exclamou, enquanto soltou as malas no chão e se afastou para observar melhor. Não era impressão, as telhas haviam sumido, podia ver o ripamento do telhado e o foro. Mas o portão estava cadeado e o muro era razoavelmente auto, as portas da casa estavam intactas e nada dentro dela sumiu. Duas perguntas lhe tiravam a paz:</p>
+
+<p>— Por que alguém roubaria telhas se era mais fácil pegar o dinheiro e as coisas que estavam dentro da casa? Como iria explicar isso para a Polícia? — Chamou as autoridades e registrou o Boletim de ocorrência, mas os policiais desconfiaram dele próprio e não levaram muito a sério a investigação. Passou a folga comprando mais telhas e ajudando os homens que contratou para cobrir a casa novamente.</p>
+
+<p>Mas logo a polícia teve de atender outro caso parecido na cidade, e em um lugar após outro começaram a sumir telhados. Nas cidades vizinhas o fenômeno também passou a acontecer. Percebeu-se que se tratava de uma quadrilha, pois em tão pouco tempo e sem despertar a atenção dos donos da casa e de vizinhos, a quadrilha destelhava uma casa em poucas horas e até em alguns minutos. </p>
+
+<p>Até que um dia a casa caiu, a polícia interceptou um caminhão com telhas roubadas de uma casa, e os detetives entraram em ação. Prenderam o chefe da quadrilha após interrogar os meliantes no caminhão. Eles tinham ficha limpa e não sabiam estar cometendo um crime, eram trabalhadores honestos que destelhavam as casas como lhes era pedido, carregavam o caminhão e as levavam até uma fazenda, de forma que os donos das casas pudessem por telhas novas. Acreditavam trabalhar em uma empresa de destelhamento.</p>
+
+<p>Ao interrogar o chefe da quadrilha, descobriram o destino das telhas, na fazenda e sem a ajuda dos empregados, o meliante triturava e peneirava o pó da argila em busca de algo precioso, e depois de peneirado o pó da argila era vendida para uma olaria e usado para fazer novos artefatos de cerâmica. A polícia não conseguiu tirar do meliante a motivação do crime, ele não revelou o que procurava no pó da argila.</p>
+
+<p>Anos depois, em uma cidade vizinha, um grupo de crianças brincando em um terreno baldio encontrou, em meio a um monte de entulho, uma pedra transparente e que brilhava, brilhava digamos, igual diamante.</p>
+
+<div class="data"><i>30/07/22</i></div>
+<hr>
+</section>
+<div class="compartilhe">
+    <h3>Compartilhe:</h3>
+    <section class="logoscomp">
+      <nav>
+        <ul>
+          <li>
+    <a href="whatsapp://send?text=Acesse: https://vacilouviroucronica.com/a-gangue-da-telha.php">
+      <img src="imagens/whatslog.png"/>
+    </a>
+</li>
+
+<li>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=https://vacilouviroucronica.com/a-gangue-da-telha.php">
+      <img src="imagens/facelog2.png"/>
+    </a>
+</li>
+<li>
+    <a href="https://telegram.me/share/url?url= https://vacilouviroucronica.com/a-gangue-da-telha.php&text= A Gangue da Telha">
+      <img src="imagens/telegramlog.png"/>
+    </a>
+</li>
+<li>
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://vacilouviroucronica.com/a-gangue-da-telha.php">
+      <img src="imagens/linkedinlog.png"/>
+    </a>
+</li>
+</ul>
+</nav>
+</section>
+<input type="text" class="url" value="https://vacilouviroucronica.com/a-gangue-da-telha.php"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script>
+    $(function(){
+    $(".url").click(function(){
+        $(this).select();
+        document.execCommand('copy');
+    })
+    
+    })
+    
+    </script>
+
+</div>
+
+  </article><!--fim da div class crônica-->
+  <aside>
+  <?php 
+  include "asidecronica.php";
+  ?>
+</aside>
+  </main><!--fim da div geral-->
+<section class="sugestoes">
+        <h3>Sugeridas:</h3>
+<nav>
+<ul>
+<a rel="dofollow" href="coisas-de-chacaco.php"> ⬅ Coisas de Cachaço</a>
+    </ul>
+    <!--ul>
+    <a rel="dofollow" href="o-mais-humilde.php"> O mais humilde ➡ </a>
+  </ul-->
+</nav>
+</section>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
