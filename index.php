0231484ae940887e8a963f18a6418ff62e4a60f8
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-09-05 23:52:09 -0300

    Teste de Conflito

--- index.php
+++ index.php
@@ -1,183 +1,31 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-<link rel="stylesheet" type="text/css" href="css/style.css">
-	  <link rel="shortcut icon" href="imagens/log.png" type="image x-icon">
-    <script type="text/java-script" src="scripts.js"></script>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="author" content="Anderson Lietz"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Vacilou, Virou Crônica</title>
-    <meta name="description" content="Toda sexta-feira uma nova crônica sobre o meu dia-a-dia, 
+<!DOCTYPE html><html lang="pt-BR"><head><link rel="stylesheet" type="text/css" href="css/style.css"><link rel="shortcut icon" href="imagens/log.png" type="image x-icon"><script type="text/java-script" src="scripts.js"></script><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="author" content="Anderson Lietz"/><meta name="viewport" content="width=device-width, initial-scale=1.0"/><title>Vacilou, Virou Crônica</title><meta name="description" content="Toda sexta-feira uma nova crônica sobre o meu dia-a-dia, 
              algumas opiniões e devaneios, entre narrativas engraçadas,
-              fofas ou trágicas." />
-    <link rel="canonical" href="https://vacilouviroucronica.com/" />
-    <meta property="og:site_name" content="Vacilou, virou Crônica! | O que vacila, vira Crônica!" />
-    <meta name="robots" content="index,follow">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://vacilouviroucronica.com/" />
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PVMJCYRXK6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PVMJCYRXK6');
-</script>
-</head>
-<body>
-          
-           <header id="headerprincipal">
-           <div id="topo"></div>
-           <h1>Vacilou, virou Crônica!</h1>
-          <input type="checkbox" id="bt_menu">
-          <label for="bt_menu">&#9776;</label>
-          <nav class="menu">
-            <ul>
-              <li><a href="area-privada.php">Biblioteca</a></li>
-              <li><a href="#contato">Contato</a></li>
-              <li><a href="#sobre">Sobre</a></li>
-            </ul>
-          </nav>
-
-           <div id="descricao">
-            <b>Toda sexta-feira uma nova crônica sobre o meu dia-a-dia, 
+              fofas ou trágicas." /><link rel="canonical" href="https://vacilouviroucronica.com/" /><meta property="og:site_name" content="Vacilou, virou Crônica! | O que vacila, vira Crônica!" /><meta name="robots" content="index,follow"><meta property="og:type" content="website"><meta property="og:url" content="https://vacilouviroucronica.com/" /><script async src="https://www.googletagmanager.com/gtag/js?id=G-PVMJCYRXK6"></script><script>
+window.dataLayer=window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());gtag('config', 'G-PVMJCYRXK6');</script></head><body><header id="headerprincipal"><div id="topo"></div><h1>Vacilou, virou Crônica!</h1><input type="checkbox" id="bt_menu"><label for="bt_menu">&#9776;</label><nav class="menu"><ul><li><a href="area-privada.php">Biblioteca</a></li><li><a href="#contato">Contato</a></li><li><a href="#sobre">Sobre</a></li></ul></nav><div id="descricao"><b>Toda sexta-feira uma nova crônica sobre o meu dia-a-dia, 
              algumas opiniões e devaneios, entre narrativas engraçadas,
-              fofas ou trágicas.</b></div>
-          
-          </header>
-         
-            <main>
-            <section class="amostras">
-              <div id="geral"></div>
-              
-              <div class="amostra">
-                          <img src="imagens/vendedor-de-cacarecos.jpg" class="imagem"/> 
-                          <h2>Vendedor de Cacarecos</h2>
-                          <div class="texto">
-                          <span>
+              fofas ou trágicas.</b></div></header><main><section class="amostras"><div id="geral"></div><div class="amostra"><img src="imagens/vendedor-de-cacarecos.jpg" class="imagem"/><h2>Vendedor de Cacarecos</h2><div class="texto"><span>
                           Alguns dias atrás tive um susto quando vi um comunicado de falecimento no facebook.
                           Não acreditei, havia algo de errado, pensei que era alguma pegadinha de primeiro de Abril, mesmo sendo início de Agosto.  
-                    </span></div>
-                      <a href="vendedor-de-cacarecos.php"><button>Ler Crônica</button></a>
-                       </div><!--fim da amostra-->
-
-              <div class="amostra">
-                          <img src="imagens/a-gangue-da-telha.jpg" class="imagem"/> 
-                          <h2>A Gangue da Telha</h2>
-                          <div class="texto">
-                          <span>
+                    </span></div><a href="vendedor-de-cacarecos.php"><button>Ler Crônica</button></a></div><div class="amostra"><img src="imagens/a-gangue-da-telha.jpg" class="imagem"/><h2>A Gangue da Telha</h2><div class="texto"><span>
                             Em uma região desconhecida, a gangue da telha colocou medo, assustou e apavorou os moradores. 
                           
-                    </span></div>
-                      <a href="a-gangue-da-telha.php"><button>Ler Crônica</button></a>
-                       </div><!--fim da amostra-->
-
-              <div class="amostra">
-                          <img src="imagens/coisas-de-cachaco.jpg" class="imagem"/> 
-                          <h2>Coisas de Cachaço</h2>
-                          <div class="texto">
-                          <span>
+                    </span></div><a href="a-gangue-da-telha.php"><button>Ler Crônica</button></a></div><div class="amostra"><img src="imagens/coisas-de-cachaco.jpg" class="imagem"/><h2>Coisas de Cachaço</h2><div class="texto"><span>
                           Como escrevi em outra crônica, minha família sempre manteve e abateu porcos.
                            Geralmente comprando os leitões quando estes eram desmamados da mãe e vendidos 
                            por outros criadores. 
-                    </span></div>
-                      <a href="coisas-de-cachaco.php"><button>Ler Crônica</button></a>
-                       </div><!--fim da amostra-->
-              
-              
-              <div class="amostra">
-                          <img src="imagens/os-dados-perdidos.jpg" class="imagem"/> 
-                          <h2>Os Dados Perdidos</h2>
-                          <div class="texto">
-                          <span>
+                    </span></div><a href="coisas-de-cachaco.php"><button>Ler Crônica</button></a></div><div class="amostra"><img src="imagens/os-dados-perdidos.jpg" class="imagem"/><h2>Os Dados Perdidos</h2><div class="texto"><span>
                           Ergueu a cabeça ainda meio tonto, observando seu próprio corpo ali deitado no asfalto.
      As costas, a cabeça, bem como várias partes do seu corpo estavam doloridas. 
      O barulho dos carros começava a aumentar novamente, ainda mais o risco de ser atropelado se
       continuasse ali dormindo na rua.
-                    </span></div>
-                      <a href="os-dados-perdidos.php"><button>Ler Crônica</button></a>
-                       </div><!--fim da amostra-->
-                       
-                       <div class="amostra">
-                          <img src="imagens/represas-e-moinhos.jpg" class="imagem"/> 
-                          <h2>Represas e Moinhos</h2>
-                          <div class="texto">
-                          <span>
+                    </span></div><a href="os-dados-perdidos.php"><button>Ler Crônica</button></a></div><div class="amostra"><img src="imagens/represas-e-moinhos.jpg" class="imagem"/><h2>Represas e Moinhos</h2><div class="texto"><span>
                           No almoço de hoje, a pauta principal foi uma região aqui do interior,
      onde há uma represa que foi feita para levar  água a um antigo moinho de grãos. 
     Do moinho ainda restam o grande galpão, e os suportes onde ficava presa a roda d'água.
-                    </span></div>
-                      <a href="represas-e-moinhos.php"><button>Ler Crônica</button></a>
-                       </div><!--fim da amostra-->
-                      
-<!--div class="amostra">
-<a href="pagina2.php">Outras...</a>
-
-</div-->
-  <div class="amostra">
-
-
-                      <details>
-                        <summary>Mais antigas:</summary>
-                        <nav>
-                       
-    <!--<ul><a href=""></a></ul>-->
-    <ul><a href="um-caso-de-engraxate.php">Um caso de Engraxate</a></ul>
-    <ul><a href="as-cronicas.php">As Crônicas</a></ul>
-    <ul><a href="garotos-machoes.php">Garotos Machões</a></ul>
-    <ul><a href="visoes-odontologicas.php">Visões Odontológicas</a></ul>
-    <ul><a href="o-mais-humilde.php">O mais humilde</a></ul>
-    <ul><a href="ventos-do-inverno.php">Ventos do Inverno</a></ul>
-    <ul><a href="a-cronica-esquecida.php">A Crônica esquecida</a></ul>
-    <ul><a href="torroes-de-argila.php">Torrões de Argila</a></ul>
-    <ul><a href="nao-quero-me-despedir.php">Não quero me despedir</a></ul>
-    <ul><a href="gafe-na-partida-de-volei.php">Gafe na partida de Vôlei</a></ul>
-    <ul><a href="epoca-de-pinhao.php">Época de Pinhão</a></ul>
-    <ul><a href="a-revolucao-das-aranhas.php">A Revolução das Aranhas</a></ul>
-    <ul><a href="resenha-de-uma-manha.php">Resenha de uma Manhã</a></ul>
-    <ul><a href="lampadas-que-queimam.php">Lâmpadas que queimam</a></ul>
-    <ul><a href="bigode-e-resistencia.php">Bigode e Resistência</a></ul>
-    <ul><a href="sejamos-realistas.php">Sejamos Realistas!</a></ul>
-    <ul><a href="culturas-em-tempos-de-guerra.php">Culturas em tempos de guerra</a></ul>
-    <ul><a href="em-um-dia-de-chuva.php">Em um dia de chuva</a></ul>
-    <ul><a href="roubo-meio-sucedido.php">Roubo meio sucedido</a></ul>
-    <ul><a href="sera-que-chove.php">Será que chove?</a></ul>
-    <ul><a href="nao-prezado-vendedor.php">Não, prezado vendedor!</a></ul>
-    <ul><a href="para-registrar.php">Para Registrar</a></ul>
-    <ul><a href="fez_tres_anos.php">Faz três anos!</a></ul>
-    <ul><a href="senhores_passageiros.php">Senhores Passageiros</a></ul>
-    <ul><a href="rotina-pacata-de-phileas-fogg.php">A rotina pacata de Phileas Fogg </a></ul>
-    <ul><a href="briques-escolares.php">Briques Escolares</a></ul>
-    <ul><a href="cronica-musical.php">Crônica Musical</a></ul>
-    <ul><a href="peixe-de-estimaçao.php">Peixe de Estimação</a></ul>
-    <ul><a href="inimigo-anonimo.php">Inimigo Anônimo</a></ul>
-    <ul><a href="reuniao-de-design.php">Reunião de Design</a></ul>
-    <ul><a href="saudades-da-chalana.php">Saudades da Chalana</a></ul>
-    <ul><a href="ladrao-de-humanas.php">Ladrão de Humanas</a></ul>
-    <ul><a href="o-fardo-de-gatsby.php">Os fardos de Jay Gatsby</a></ul>
-    <ul><a href="minha-padaria.php">Minha Padaria</a></ul>
-    <ul><a href="daqui-cinquenta-e-nove-anos.php">Daqui Cinquenta e nove anos...</a></ul>
-    <ul><a href="procura-se.php">Procura-se </a></ul>
-
-                        </nav>
-                      </details>
-</div>
-                      </section>
-      
-                      <aside>
-  <?php
+                    </span></div><a href="represas-e-moinhos.php"><button>Ler Crônica</button></a></div><div class="amostra"><details><summary>Mais antigas:</summary><nav><!--<ul><a href=""></a></ul>--><ul><a href="um-caso-de-engraxate.php">Um caso de Engraxate</a></ul><ul><a href="as-cronicas.php">As Crônicas</a></ul><ul><a href="garotos-machoes.php">Garotos Machões</a></ul><ul><a href="visoes-odontologicas.php">Visões Odontológicas</a></ul><ul><a href="o-mais-humilde.php">O mais humilde</a></ul><ul><a href="ventos-do-inverno.php">Ventos do Inverno</a></ul><ul><a href="a-cronica-esquecida.php">A Crônica esquecida</a></ul><ul><a href="torroes-de-argila.php">Torrões de Argila</a></ul><ul><a href="nao-quero-me-despedir.php">Não quero me despedir</a></ul><ul><a href="gafe-na-partida-de-volei.php">Gafe na partida de Vôlei</a></ul><ul><a href="epoca-de-pinhao.php">Época de Pinhão</a></ul><ul><a href="a-revolucao-das-aranhas.php">A Revolução das Aranhas</a></ul><ul><a href="resenha-de-uma-manha.php">Resenha de uma Manhã</a></ul><ul><a href="lampadas-que-queimam.php">Lâmpadas que queimam</a></ul><ul><a href="bigode-e-resistencia.php">Bigode e Resistência</a></ul><ul><a href="sejamos-realistas.php">Sejamos Realistas!</a></ul><ul><a href="culturas-em-tempos-de-guerra.php">Culturas em tempos de guerra</a></ul><ul><a href="em-um-dia-de-chuva.php">Em um dia de chuva</a></ul><ul><a href="roubo-meio-sucedido.php">Roubo meio sucedido</a></ul><ul><a href="sera-que-chove.php">Será que chove?</a></ul><ul><a href="nao-prezado-vendedor.php">Não, prezado vendedor!</a></ul><ul><a href="para-registrar.php">Para Registrar</a></ul><ul><a href="fez_tres_anos.php">Faz três anos!</a></ul><ul><a href="senhores_passageiros.php">Senhores Passageiros</a></ul><ul><a href="rotina-pacata-de-phileas-fogg.php">A rotina pacata de Phileas Fogg </a></ul><ul><a href="briques-escolares.php">Briques Escolares</a></ul><ul><a href="cronica-musical.php">Crônica Musical</a></ul><ul><a href="peixe-de-estimaçao.php">Peixe de Estimação</a></ul><ul><a href="inimigo-anonimo.php">Inimigo Anônimo</a></ul><ul><a href="reuniao-de-design.php">Reunião de Design</a></ul><ul><a href="saudades-da-chalana.php">Saudades da Chalana</a></ul><ul><a href="ladrao-de-humanas.php">Ladrão de Humanas</a></ul><ul><a href="o-fardo-de-gatsby.php">Os fardos de Jay Gatsby</a></ul><ul><a href="minha-padaria.php">Minha Padaria</a></ul><ul><a href="daqui-cinquenta-e-nove-anos.php">Daqui Cinquenta e nove anos...</a></ul><ul><a href="procura-se.php">Procura-se </a></ul></nav></details></div></section><aside><?php
   include "asideindex.php";
-  ?>
-</aside>
-                     
-                      </main>
-                      <section id="sobre">
-                    <h2>Quem sou eu?</h2>
-                        <img src="imagens/perfil1.jpg" class="imagem">
-                        <p>Me chamo Anderson Lietz e tenho 21 anos. Moro no interior de Porto União, SC,
+  ?></aside></main><section id="sobre"><h2>Quem sou eu?</h2><img src="imagens/perfil1.jpg" class="imagem"><p>Me chamo Anderson Lietz e tenho 21 anos. Moro no interior de Porto União, SC,
                            faço graduação à distância em Análise e desenvolvimento de sistemas, 
                           e atualmente estou em busca de um estágio também à distância.
                            Me interessei pela escrita criativa no final do ensino médio em 2018, 
@@ -187,47 +35,4 @@
                           Este site foi desenvolvido por mim 
                           com a intenção de aprender programação e algumas tecnologias na prática.
                            
-                      </p>
-                     </section>
-<!--/div--><!--Fim da div geral-->
-                    <div class="contato">
-                      <div id="contato"></div>
-                      <section id="footer">
-            <h2>Contato</h2>
-              
-                        <div id="links">
-                                  <div class="blocos">
-                                    <img src="imagens/email1.png" class="imagem">
-                                    <a href="mailto:contato@vacilouviroucronica.com">
-                                      <p>@e-mail</p></a>
-                                    </div>
-                          
-                                      <div class="blocos">
-                                      
-                                      
-                                        <img src="imagens/insta2.png" class="imagem">
-                                        <a href="https://www.instagram.com/vacilouviroucronica">
-                                          <p>@vacilouviroucronica</p></a>
-                                      </div>
-                          </div>
-          </section>
-                     </div>
-                     </div>
-                     <a href="#topo"><div class="btopo">Voltar ao topo</div></a>
-                     <nav class="linksrodape">
-                       <ul>
-                         <li>
-                            <a href="#">Política de Privacidade</a>
-                        </li>
-                        <li>
-                            <a href="#">Termos de Uso</a>
-                        </li>
-                        <li>
-                            <a href="https://lietz.dev.br">Desenvolvimento</a>
-                        </li>
-                      </ul>
-                    </nav>
-                    <div class="copy">&copy; 2021-2022 Vacilou, Virou Crônica! - Todos os direitos reservados. </div>
-                    
-        </body>
-</html>
+                      </p></section><!--/div--><div class="contato"><div id="contato"></div><section id="footer"><h2>Contato</h2><div id="links"><div class="blocos"><img src="imagens/email1.png" class="imagem"><a href="mailto:contato@vacilouviroucronica.com"><p>@e-mail</p></a></div><div class="blocos"><img src="imagens/insta2.png" class="imagem"><a href="https://www.instagram.com/vacilouviroucronica"><p>@vacilouviroucronica</p></a></div></div></section></div></div><a href="#topo"><div class="btopo">Voltar ao topo</div></a><nav class="linksrodape"><ul><li><a href="#">Política de Privacidade</a></li><li><a href="#">Termos de Uso</a></li><li><a href="https://lietz.dev.br">Desenvolvimento</a></li></ul></nav><div class="copy">&copy; 2021-2022 Vacilou, Virou Crônica! - Todos os direitos reservados. </div></body></html> 
