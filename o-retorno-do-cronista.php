7d27cda73121f1188705e5ad8c24359141645ae6
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-10-07 20:01:06 -0300

    Alterações no css e o-retorno-do-cronista.php

---
+++ o-retorno-do-cronista.php
@@ -0,0 +1,128 @@
+<head>
+    <link rel="shortcut icon" href="imagens/log.png" type="image x-icon">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description"content="Cá estou, sentado com o computador na mesa da cozinha, escrevendo mais uma crônica. Não escrevo uma crônica comum, é a crônica de retorno, de abraço forte, tapinha nas costas e carinho nos cabelos. ">
+        <meta property="og:site_name" content="Vacilou, virou Crônica! | O que vacila, vira Crônica!" />
+    <meta property="og:url" content="https://vacilouviroucronica.com/o-retorno-do-cronista.php" />
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://vacilouviroucronica.com/o-retorno-do-cronista.php" />
+    <meta name="author" content="Anderson Lietz">
+    <meta property="og:type" content="article" />
+<title> O Retorno do Cronista | Vacilou, virou Crônica!</title>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+</head>
+<body>
+  <header>
+    <?php include "header.php";
+    ?>
+</header>
+              <main>
+                  
+              <article>
+                      <h1>O Retorno do Cronista</h1>
+                      <img src="imagens/o-retorno-do-cronista.jpg" class="imagem">
+                       
+   <section class="aumentarediminuirfonte">
+  <!----><!-- DIMINUIR FONTE -->
+  <span id="diminuir-fonte">
+<a title="Diminuir Fonte" alt="Diminuir">A-</a>
+</span>
+
+<!-- AUMENTAR FONTE -->
+<span id="aumentar-fonte">
+<a title="Aumentar Fonte" alt="Aumentar">A+</a>
+</span>
+</section>
+    <div class="espaco"></div>
+<section id="content">
+<p>Cá estou, sentado com o computador na mesa da cozinha, escrevendo mais uma crônica. Não escrevo uma crônica comum, é a crônica de retorno, de abraço forte, tapinha nas costas e carinho nos cabelos. Um retorno que não seria preciso, pois eu não fui a lugar nenhum, apenas deixei por um momento (quase dois meses), de expor meus devaneios e lembranças neste cantinho da internet. Talvez depois desse tempo eu seja apenas um desconhecido para você, um mero mortal escrevendo palavras vazias na areia da praia, ou rascunhando o vento. Mas se sentiu minha falta, sinta-se abraçado, e saiba que voltei aqui por você.</p>
+
+<p>Mas afinal, por que eu deixei de escrever? Devo confessar, é uma boa pergunta. Tudo começou quando passei a me dedicar escrevendo mais do que uma crônica por semana, a fim de ter crônicas exclusivas e talvez no futuro publicá-las em um livro. Passei a me sentir mal pois acreditava ter tantas outras coisas importantes a serem feitas e eu estava lá, escrevendo.</p>
+
+ <p>Então em uma sexta-feira decidi que precisava escrever uma nova crônica a fim de publicá-la, e a um tempo permanecia em minha mente uma ideia de enredo, e comecei a dar os nomes e escrever as cenas. Criei a página e publiquei no servidor, mas não indexei na página inicial do site. Mandei o link a um amigo que avalia as crônicas e recebi seus elogios. Mas percebi que a história estava incompleta, eu não podia deixar de lado o restante, pois a ideia era ainda mais incrível.</p>
+
+<p>Não queria postar um conto pela metade, e já estava no dia da publicação, o que me deixou ansioso. Eu não queria falhar, mas estava falhando. A semana passou e eu não publiquei nada. Outra semana veio, eu tinha esperança de então escrever duas crônicas, a daquela semana e outra para recuperar a lacuna da semana anterior. Mas não me ocorreu ideia, ou eu não quis aprimorar as que me apareceram.</p> 
+
+<p>No fundo me sentia cada vez mais ansioso, pois meu motivo de orgulho era postar crônicas toda semana, ainda mais faltando poucos meses para concluir um ano de crônicas semanais. E passava semana após semana e eu adiava em cada uma delas o meu retorno. Não foram muitos os que sentiram minha falta, ou pelo menos, que comunicaram isso. Mas houve quem percebeu o meu sumiço e me incentivou a voltar a escrever.</p>
+
+ <p>E como disse Franz Kafka: “ De um certo ponto adiante não há mais retorno. Esse é o ponto que deve ser alcançado”. Sinto que alcancei esse ponto, pois, por mais que escrever crônicas exija meu tempo e esforços, me faz ter certa medida de realização, por repartir alguns acontecimentos e ideias com outros e me dar uma sensação de produtividade, por registrar e materializar essas ideias.</p>
+
+<p>E no fundo continuo sem assunto, mas como geralmente escrevo sobre escrever crônicas quando não tenho outra coisa a escrever, aqui está uma crônica sobre a escrita de crônicas, ou melhor, sobre o meu retorno a esse assunto, pois decidi dar uma explicação e iniciar uma nova era, provavelmente de crônicas melhores. E agora que quebrei o gelo, espero publicar novamente toda semana. E posso garantir uma coisa:</p>
+
+<p>Como é bom estar de volta!</p>
+
+<div class="data"><i>07/10/22</i></div>
+<hr>
+</section>
+<div class="compartilhe">
+    <h3>Compartilhe:</h3>
+    <section class="logoscomp">
+      <nav>
+        <ul>
+          <li>
+    <a href="whatsapp://send?text=Acesse: https://vacilouviroucronica.com/o-retorno-do-cronista.php">
+      <img src="imagens/whatslog.png"/>
+    </a>
+</li>
+
+<li>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=https://vacilouviroucronica.com/o-retorno-do-cronista.php">
+      <img src="imagens/facelog2.png"/>
+    </a>
+</li>
+<li>
+    <a href="https://telegram.me/share/url?url= https://vacilouviroucronica.com/o-retorno-do-cronista.php&text= O Retorno do Cronista">
+      <img src="imagens/telegramlog.png"/>
+    </a>
+</li>
+<li>
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://vacilouviroucronica.com/o-retorno-do-cronista.php">
+      <img src="imagens/linkedinlog.png"/>
+    </a>
+</li>
+</ul>
+</nav>
+</section>
+<input type="text" class="url" value="https://vacilouviroucronica.com/o-retorno-do-cronista.php"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script>
+    $(function(){
+    $(".url").click(function(){
+        $(this).select();
+        document.execCommand('copy');
+    })
+    
+    })
+    
+    </script>
+
+</div>
+
+  </article><!--fim da div class crônica-->
+  <aside>
+  <?php 
+  include "asidecronica.php";
+  ?>
+</aside>
+  </main><!--fim da div geral-->
+<section class="sugestoes">
+        <h3>Sugeridas:</h3>
+<nav>
+<ul>
+<a rel="dofollow" href="vendedor-de-cacarecos.php"> ⬅ Vendedor de Cacarecos</a>
+    </ul>
+    <ul>
+    <!--a rel="dofollow" href="fim-de-tarde.php"> Fim de Tarde ➡ </a-->
+  </ul>
+</nav>
+</section>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
