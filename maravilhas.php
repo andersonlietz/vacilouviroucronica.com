78cb08014aa95461ba76ee0800caf9624db0c794
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-11-09 16:25:09 -0300

    post maravilhas.php

---
+++ maravilhas.php
@@ -0,0 +1,119 @@
+<head>
+    <link rel="shortcut icon" href="imagens/log.png" type="image x-icon">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"content="">
+    <meta name="author" content="Anderson Lietz">
+    <meta property="og:site_name" content="Vacilou, virou Crônica! | O que vacila, vira Crônica!" />
+    <link rel="canonical" href="https://vacilouviroucronica.com/maravilhas.php" />
+    <meta property="og:url" content="https://vacilouviroucronica.com/maravilhas.php" />
+    <meta name="robots" content="index,follow">
+    <meta property="og:type" content="article" />
+<title>Maravilhas | Vacilou, virou Crônica!</title>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+  <header>
+    <?php
+      include "header.php";
+    ?>
+</header>
+              <main>
+                  
+              <article>
+                      <h2>Maravilhas</h2>
+	
+	<img src="imagens/maravilhas.jpg" class="imagem">Foto de Глеб Коровко no Pexels</img>
+   <section class="aumentarediminuirfonte">
+  <!----><!-- DIMINUIR FONTE -->
+<span id="diminuir-fonte">
+<a title="Diminuir Fonte" alt="Diminuir">A-</a>
+</span>
+
+<!-- AUMENTAR FONTE -->
+<span id="aumentar-fonte">
+<a title="Aumentar Fonte" alt="Aumentar">A+</a>
+</span>
+</section>
+    <div class="espaco"></div>
+<section id="content">
+
+
+
+
+
+    <div class="data"><i>08/04/22</i></div>
+<hr>
+</section>
+<div class="compartilhe">
+    <h3>Compartilhe:</h3>
+    <section class="logoscomp">
+      <nav>
+        <ul>
+          <li>
+    <a href="whatsapp://send?text=Acesse: https://vacilouviroucronica.com/a-revolucao-das-aranhas.php">
+      <img src="imagens/whatslog.png"/>
+    </a>
+</li>
+
+<li>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=https://vacilouviroucronica.com/a-revolucao-das-aranhas.php">
+      <img src="imagens/facelog2.png"/>
+    </a>
+</li>
+<li>
+    <a href="https://telegram.me/share/url?url= https://vacilouviroucronica.com/a-revolucao-das-aranhas.php&text= A Revolução das Aranhas">
+      <img src="imagens/telegramlog.png"/>
+    </a>
+</li>
+<li>
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://vacilouviroucronica.com/a-revolucao-das-aranhas.php">
+      <img src="imagens/linkedinlog.png"/>
+    </a>
+</li>
+</ul>
+</nav>
+
+
+</section>
+    <input type="text" class="url" value="https://vacilouviroucronica.com/a-revolucao-das-aranhas.php"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+ <script>
+        $(function(){
+    $(".url").click(function(){
+        $(this).select();
+        document.execCommand('copy');
+    })
+    
+    })
+    
+    </script>
+
+</div>
+
+  </article><!--fim do  artigo-->
+  <aside>
+  <?php 
+  include "asidecronica.php";
+  ?>
+</aside>
+  </main><!--fim da div geral-->
+<section class="sugestoes">
+        <h3>Sugeridas:</h3>
+<nav>
+<ul>
+<a href="resenha-de-uma-manha.php"> ⬅ Resenha de uma Manhã </a>
+    </ul>
+    <ul>
+    <a href="epoca-de-pinhao.php"> Época de Pinhão ➡ </a>
+</ul>
+   
+</nav>
+</section>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
